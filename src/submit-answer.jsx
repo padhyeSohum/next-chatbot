8074cbb2252698ac26b93a019108bfe24ffa3650
padhyeSohum <sohum.padhye@gmail.com>
2022-04-30 23:02:46 -0400

    made helper text show up at the start for submit-answerw

--- src/submit-answer.jsx
+++ src/submit-answer.jsx
@@ -12,76 +12,81 @@ const SubmitAnswer = (props) => {
         return;
     }
 
-    const [answerToQuestion, setAnswerToQuestion] = useState('');
-    const [submissionResponse, setSubmissionResponse] = useState(null);
-    const [helperTextQuestion, setHelperTextQuestion] = useState('');
-    const [validationStatusQuestion, setValidationStatusQuestion] = useState(false);
-    const [helperTextAnswer, setHelperTextAnswer] = useState('');
-    const [validationStatusAnswer, setValidationStatusAnswer] = useState(false);
-    const [overallValidation, setOverallValidation] = useState(false);
-    const [isSuccess, setIsSuccess] = useState(false);
-    const [questionAsked, setQuestionAsked] = useState(props.ongoingQuery);
-
-
-    const handleChangeQuestion = (event) => {
-        let tempQuestion = event.target.value;
+    const getHelperTextQuestion = (currentQuestion) => {
+        
         let tempHelperTextQuestion = '';
-        let tempValidationStatusQuestion = false;
 
-        if (tempQuestion === '') {
+        if (currentQuestion === '') {
             tempHelperTextQuestion = 'Please fill out this field!';
         }
 
-        else if (tempQuestion.length < 7) {
+        else if (currentQuestion.length < 7) {
             tempHelperTextQuestion = 'Please make question longer';
         }
     
-        else if (tempQuestion.length > 20) {
+        else if (currentQuestion.length > 20) {
             tempHelperTextQuestion = 'Please make question shorter';
         }
-        else {
-            tempValidationStatusQuestion = true;
-        }
 
-        setQuestionAsked(tempQuestion);
-        setHelperTextQuestion(tempHelperTextQuestion);
-        setValidationStatusQuestion(tempValidationStatusQuestion);
-        setOverallValidation(tempValidationStatusQuestion && validationStatusAnswer)
+        return tempHelperTextQuestion;
 
     }
 
-    const handleChangeAnswer = (event) => {
-        let tempAnswer = event.target.value;
-        let tempHelperTextAnswer = '';
-        let tempValidationStatusAnswer = false;
+    const handleChangeQuestion = (currentQuestion) => {
+        setQuestion(currentQuestion);
 
-        if (tempAnswer === '') {
+        setHelperTextQuestion(getHelperTextQuestion(currentQuestion));
+        setIsValidQuestion(getIsValidQuestion(currentQuestion));
+    }
+
+    const getIsValidQuestion = (currentQuestion) => {
+        return getHelperTextQuestion(currentQuestion).length === 0
+    }
+
+    const getHelperTextAnswer = (currentAnswer) => {
+        
+        let tempHelperTextAnswer = '';
+
+        if (currentAnswer === '') {
             tempHelperTextAnswer = 'Please fill out this field!';
         }
-    
-        else if (tempAnswer.length < 7) {
+
+        else if (currentAnswer.length < 7) {
             tempHelperTextAnswer = 'Please make answer longer';
         }
     
-        else if (tempAnswer.length > 20) {
+        else if (currentAnswer.length > 20) {
             tempHelperTextAnswer = 'Please make answer shorter';
         }
-        else {
-            tempValidationStatusAnswer = true;
-        }
-    
-        setAnswerToQuestion(tempAnswer);
-        setHelperTextAnswer(tempHelperTextAnswer);
-        setValidationStatusAnswer(tempValidationStatusAnswer);
-        setOverallValidation(tempValidationStatusAnswer && validationStatusQuestion);
+
+        return tempHelperTextAnswer;
 
     }
+
+    const handleChangeAnswer = (currentAnswer) => {
+        setAnswer(currentAnswer);
+
+        setHelperTextAnswer(getHelperTextAnswer(currentAnswer));
+        setIsValidAnswer(getIsValidAnswer(currentAnswer));
+    }
+
+    const getIsValidAnswer = (currentAnswer) => {
+        return getHelperTextAnswer(currentAnswer).length === 0
+    }
+
+    const [question, setQuestion] = useState(props.ongoingQuery);
+    const [helperTextQuestion, setHelperTextQuestion] = useState(getHelperTextQuestion(props.ongoingQuery));
+    const [isValidQuestion, setIsValidQuestion] = useState(getIsValidQuestion(props.ongoingQuery));
+
+    const [answer, setAnswer] = useState('');
+    const [helperTextAnswer, setHelperTextAnswer] = useState(getHelperTextAnswer(''));
+    const [isValidAnswer, setIsValidAnswer] = useState(getIsValidAnswer(''))
 
     const handleOnSubmit = () => {
         axios.post(window.origin + "/api/submit-answer", 
             {
-                question: questionAsked,
-                answer: answerToQuestion
+                question: question,
+                answer: answer
 
             }
         ).then((response) => {
@@ -104,13 +109,13 @@ const SubmitAnswer = (props) => {
 
     return (
         <div className = {styles.submitAnswer}>
-            <TextField error = {!validationStatusQuestion} helperText = {helperTextQuestion} sx={{}} required label = "Your Question" defaultValue = {questionAsked} value = {questionAsked} variant = "outlined" onChange = {handleChangeQuestion}/>
-            <TextField error = {!validationStatusAnswer} helperText = {helperTextAnswer} sx={{margin: "10px 0"}} required label = "Your Answer" defaultValue="" value = {answerToQuestion} variant = "outlined" onChange = {handleChangeAnswer} />
+            <TextField error = {!isValidQuestion} helperText = {helperTextQuestion} sx={{}} required label = "Your Question" value = {question} variant = "outlined" onChange = {(e) => handleChangeQuestion(e.target.value)}/>
+            <TextField error = {!isValidAnswer} helperText = {helperTextAnswer} sx={{margin: "10px 0"}} required label = "Your Answer" value = {answer} variant = "outlined" onChange = {(e) => handleChangeAnswer(e.target.value)} />
             {/* <input type = "text" className = "submit-answer-input" value = {this.props.questionAsked} readOnly/>
             <input type = "text" className = "submit-answer-input" value = {this.state.questionAsked} onChange = {this.handleChange} required /> */}
 
             <div className={styles.submitAnswerButtonsContainer}>
-                <Button disabled = {!overallValidation} onClick = {handleOnSubmit} variant = "contained" color = "primary">Submit</Button>
+                <Button disabled = {!isValidQuestion || !isValidAnswer} onClick = {handleOnSubmit} variant = "contained" color = "primary">Submit</Button>
                 <Button onClick = {handleOnCancel} variant = "contained" color = "secondary">Cancel</Button>
             </div>
             {/* <button type = "button" onClick = {this.handleOnSubmit}>Submit</button> */}
